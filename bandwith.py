5d872f6066f04145ac3c80b285a9284484bb9b3d
jonas n <jonasneumeier@gmx.de>
2022-12-05 17:00:54 +0100

    read and plot data

--- bandwith.py
+++ bandwith.py
@@ -1,4 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-with
+file = 'vna_remote.txt'
+
+with open(file, "r") as lines:
+    lines = lines.readlines()[1:]
+    freq = [float(x.split(' ')[0]) for x in lines]
+    s21 = [float(x.split(' ')[2]) for x in lines]
+
+print(freq[1])
+print(s21[1])
+print(len(freq))
+print(len(s21))
+
+plt.figure()
+plt.plot(freq,s21)
+plt.show()
+
